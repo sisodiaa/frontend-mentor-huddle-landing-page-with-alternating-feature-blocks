0ffd5c6c21bc89e9530a9003fc8812fad43daf10
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-06-22 23:16:25 +0530

    Change configuration value for

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sisodiaa.github.io/",
+  site: "https://sisodiaa.github.io",
   repo: "/frontend-mentor-huddle-landing-page-with-alternating-feature-blocks",
   experimental: {
     assets: true,
