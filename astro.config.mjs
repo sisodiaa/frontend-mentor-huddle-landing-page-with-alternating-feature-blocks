dc1f08be48d331d25e3bc71e20dad5cc61adf303
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-06-22 18:00:58 +0530

    Opt for experimental assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://sisodiaa.github.io/",
   repo: "/frontend-mentor-huddle-landing-page-with-alternating-feature-blocks",
+  experimental: {
+    assets: true,
+  },
 });
