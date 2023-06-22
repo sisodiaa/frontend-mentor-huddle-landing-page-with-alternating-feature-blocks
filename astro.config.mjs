89a00bdf592ecbf377f8fef6943cd44d6d3e97ce
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-06-22 17:41:50 +0530

    Add configuration for Astro

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://sisodiaa.github.io/",
+  repo: "/frontend-mentor-huddle-landing-page-with-alternating-feature-blocks",
+});
