9c79c59b1e8a3a87d5af3804c752aaff25984d8c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-06-25 11:51:42 +0530

    Change `repo` to `base`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://sisodiaa.github.io",
-  repo: "/frontend-mentor-huddle-landing-page-with-alternating-feature-blocks",
+  base: "/frontend-mentor-huddle-landing-page-with-alternating-feature-blocks",
   experimental: {
     assets: true,
   },
